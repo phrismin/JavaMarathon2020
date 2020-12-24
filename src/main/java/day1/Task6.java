456835c8d767dad1a3baef27c912fcbe6df348e1
Рудой А.Ю <rudoy_a@mzkt.by>
2020-12-24 09:46:19 +0300

    Add solution for day1: Tasks1-6

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,12 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        int k = 2;
+        int i = 1;
+        while (i < 10) {
+            int mult = i * k;
+            System.out.println(i + " x " + k + " = " +mult);
+            i++;
+        }
     }
 }
