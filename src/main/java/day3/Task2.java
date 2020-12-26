2bd6f7ce01381074f4b09e932acad79dfb85ddcb
Alex Rudoi <rudenw01@gmail.com>
2020-12-26 11:49:23 +0300

    Add day 3 solution

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,31 @@
 package day3;
 
+//1 2 - ваш ввод в консоль
+//        0.5 - ответ программы
+//        100 77 - ваш ввод в консоль
+//        1.2987012987012987 - ответ программы
+//        3 0 - ваш ввод в консоль
+//        работа программы завершается
+
+
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        double a;
+        double b;
+        double res;
+
+        while (true) {
+            a = scan.nextDouble();
+            b = scan.nextDouble();
+            if (b == 0)
+                break;
+            res = a / b;
+            System.out.println(res);
+        }
 
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,25 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        double a;
+        double b;
+
+        for (int i = 0; i < 5; i++) {
+            a = scan.nextDouble();
+            b = scan.nextDouble();
+
+            if (b == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+
+            System.out.println(a / b);
+        }
 
     }
 }
