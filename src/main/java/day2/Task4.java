d829736807616fd5215edd732d522943ea9fa45f
Alex Rudoi <rudenw01@gmail.com>
2020-12-26 09:29:17 +0300

    Update solution for Task4 day2

--- src/main/java/day2/Task4.java
+++ src/main/java/day2/Task4.java
@@ -8,18 +8,15 @@ public class Task4 {
 
         Scanner scan = new Scanner(System.in);
         double x = scan.nextDouble();
+        double y = 0;
 
-        if (x >= 5) {
-            double y = (pow(x, 2) - 10) / (x + 7);
-            System.out.println(y);
-        }
+        if (x >= 5)
+            y = (pow(x, 2) - 10) / (x + 7);
+        else if (x > -3 & x < 5)
+            y  = (x + 3) * (pow(x, 2) - 2);
+        else y = 420.0;
 
-        else if (x > -3 & x < 5) {
-            double y  = (x + 3) * (pow(x, 2) - 2);
-            System.out.println(y);
-        }
-
-        else System.out.println(420.0);
+        System.out.println(y);
 
     }
 }
