15a818dbceda7674d7053096ff62b4b28d2f0ec2
Alex Rudoi <rudenw01@gmail.com>
2020-12-27 15:42:38 +0300

    Update solution Task1 day3

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -6,38 +6,45 @@ public class Task1 {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        String city = scan.nextLine();
 
-        switch (city) {
-            case "Москва":
-            case "Владивосток":
-            case "Ростов":
-                System.out.println("Россия");
+        while (true) {
+            String city = scan.nextLine();
+
+            if (city.equals("Stop"))
                 break;
 
-            case "Рим":
-            case "Милан":
-            case "Турин":
-                System.out.println("Италия");
-                break;
+            switch (city) {
+                case "Москва":
+                case "Владивосток":
+                case "Ростов":
+                    System.out.println("Россия");
+                    break;
 
-            case "Берлин":
-            case "Мюнхен":
-            case "Кёльн":
-                System.out.println("Германия");
-                break;
+                case "Рим":
+                case "Милан":
+                case "Турин":
+                    System.out.println("Италия");
+                    break;
 
-            case "Ливерпуль":
-            case "Манчестер":
-            case "Лондон":
-                System.out.println("Англия");
-                break;
+                case "Берлин":
+                case "Мюнхен":
+                case "Кёльн":
+                    System.out.println("Германия");
+                    break;
 
-            case "Stop":
-                break;
+                case "Ливерпуль":
+                case "Манчестер":
+                case "Лондон":
+                    System.out.println("Англия");
+                    break;
 
-            default:
-                System.out.println("Неизвестная страна");
+                case "Stop":
+                    break;
+
+                default:
+                    System.out.println("Неизвестная страна");
+            }
+
         }
     }
 }
