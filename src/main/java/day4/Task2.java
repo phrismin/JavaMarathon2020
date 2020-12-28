b06dbf90de6ce947fbe2c0cded3f5f5afe330cf7
Alex Rudoi <rudenw01@gmail.com>
2020-12-28 11:31:08 +0300

    Add solution Task2 day4

--- src/main/java/day4/Task2.java
+++ src/main/java/day4/Task2.java
@@ -1,7 +1,40 @@
 package day4;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Task2 {
     public static void main(String[] args) {
+
+        Random rand = new Random();
+        int[] arr = new int[100];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(9999) + 1;
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+        int max = arr[0];
+        int min = arr[0];
+        int count0 = 0;
+        int sum0 = 0;
+
+        for (int i: arr) {
+            if (max < i)
+                max = i;
+            if (min > i)
+                min = i;
+            if (i % 10 == 0) {
+                count0++;
+                sum0 += i;
+            }
+        }
+
+        System.out.println(min);
+        System.out.println(max);
+        System.out.println(count0++);
+        System.out.println(sum0);
 
     }
 }
