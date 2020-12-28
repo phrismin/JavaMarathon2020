f04840d7e75de8aed636269355624584301d209c
Alex Rudoi <rudenw01@gmail.com>
2020-12-28 13:54:35 +0300

    Update solution Task3 day4

--- src/main/java/day4/Task3.java
+++ src/main/java/day4/Task3.java
@@ -14,7 +14,7 @@ public class Task3 {
         for (int i = 0; i < arr.length; i++) {
             int sumString = 0;
             for (int j = 0; j < arr[i].length; j++) {
-                arr[i][j] = rand.nextInt(50) + 0;
+                arr[i][j] = rand.nextInt(49) + 0;
                 sumString += arr[i][j];
             }
             if (sumString > maxSumString) {
