363ec08d65048c4906ef26f86e571a0334d7fcbd
Alex Rudoi <rudenw01@gmail.com>
2020-12-29 09:58:45 +0300

    Update solution Task2 day5

--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -1,20 +1,10 @@
 package day5;
-//2. Создать класс Мотоцикл (англ. Motorbike) с полями “Год выпуска”, “Цвет”, “Модель”.
-// Создать экземпляр класса Мотоцикл, используя конструктор (set методы не использовать).
-// Необходимо придерживаться принципов инкапсуляции и использовать ключевое слово this.
-// Вывести в консоль значение каждого из полей, используя get методы.
 
 public class Task2 {
     public static void main(String[] args) {
 
-        Motorbike honda = new Motorbike();
-        honda.setYear(2015);
-        honda.setColor("Red");
-        honda.setModel("Vulcan");
-
-        System.out.println(honda.getColor());
-        System.out.println(honda.getModel());
-        System.out.println(honda.getYear());
+        Motorbike honda = new Motorbike(2015, "Red", "Vulcan");
+        System.out.println(honda.getYear() + ", " + honda.getColor() + ", " + honda.getModel());
 
     }
 }
@@ -25,12 +15,14 @@ class Motorbike {
     private String color;
     private String model;
 
-    public void setYear(int year) { this.year = year; }
-    public void setColor(String color) { this.color = color; }
-    public void setModel(String model) { this.model = model; }
+    public Motorbike(int year, String model, String color) {
+        this.year = year;
+        this.model = model;
+        this.color = color;
+    }
 
-    public int getYear() { return year; }
-    public String getModel() { return model; }
-    public String getColor() { return color; }
+    public int getYear() { return this.year; }
+    public String getColor() { return this.color; }
+    public String getModel() { return this.model; }
 
 }
