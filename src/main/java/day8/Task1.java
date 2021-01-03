55046aaee137399fee21be2f465d1d292e90de1a
Alex Rudoi <rudenw01@gmail.com>
2021-01-03 14:04:00 +0300

    Add solutions Task1-2 day8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,22 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long start = System.nanoTime();
+        String str1 = new String();
+        for (int i = 0; i <= 20000; i++) {
+            str1 += " " + i;
+        }
+//        System.out.println(str1);
+        long end = System.nanoTime();
+        System.out.println(end - start);
 
+        start = System.nanoTime();
+        StringBuilder str2 = new StringBuilder();
+        for (int i = 0; i <= 20000; i++) {
+            str2.append(i).append(" ");
+        }
+//        System.out.println(str2);
+        end = System.nanoTime();
+        System.out.println(end - start);
     }
 }
