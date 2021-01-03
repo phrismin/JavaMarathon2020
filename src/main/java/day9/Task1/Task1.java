952f89f3750aea6ccd6665b464e6ccacbed89dba
Alex Rudoi <rudenw01@gmail.com>
2021-01-03 23:01:05 +0300

    Add solutions Task1 day9

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -2,6 +2,12 @@ package day9.Task1;
 
 public class Task1 {
     public static void main(String[] args) {
+        Student student = new Student("Lyoha", "Marcsisty");
+        Teacher teacher = new Teacher("Lenin", "Politilogy");
+        System.out.println(student.getGroupName());
+        System.out.println(teacher.getSubjectName());
+        student.printInfo();
+        teacher.printInfo();
 
     }
 }
