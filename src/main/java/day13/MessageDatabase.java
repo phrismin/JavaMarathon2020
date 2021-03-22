50f9b493860b25e3e5cfe4503927de8e62ade9ea
phrismin <rudenw01@gmail.com>
2021-03-22 16:13:07 +0300

    solution: add project 'Social network' day 13

---
+++ src/main/java/day13/MessageDatabase.java
@@ -0,0 +1,18 @@
+package day13;
+
+import java.util.List;
+
+public class MessageDatabase {
+    public static List<Message> messages = null;
+
+    public static void addNewMessage(User u1, User u2, String text) {
+    }
+
+    public static List<Message> getMessages() {
+        return messages;
+    }
+
+    public static void showDialog(User u1, User u2) {
+
+    }
+}
--- src/main/java/day13/Task1.java
+++ src/main/java/day13/Task1.java
@@ -1,7 +1,21 @@
 package day13;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class Task1 {
     public static void main(String[] args) {
 
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, d MMMM yyyy");
+        Calendar calendar = new GregorianCalendar(2017, Calendar.JANUARY, 25);
+        calendar.set(Calendar.HOUR, 10);
+        calendar.set(Calendar.MINUTE, 42);
+        calendar.set(Calendar.SECOND, 12);
+
+        calendar.roll(Calendar.MONTH, -2);
+        System.out.println(dateFormat.format(calendar.getTime()));
     }
 }
