3a807fc7e2de4a46d3dfb65c468d2ce755a23a26
Alex Rudoi <rudenw01@gmail.com>
2021-03-25 22:20:51 +0300

    solution: add task2-3 for day14

--- src/main/java/day14/Task2.java
+++ src/main/java/day14/Task2.java
@@ -1,7 +1,39 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        String sep = File.separator;
+        String path = "src" + sep + "main" + sep + "resources" + sep + "people.txt";
 
+        System.out.println(parseFileToStringList(new File(path)));
+    }
+
+    public static List<String> parseFileToStringList(File file) {
+        List<String> list = new ArrayList<>();
+        try (Scanner scanner = new Scanner(file)) {
+            String[] arrayStr;
+            String currentStr;
+            while (scanner.hasNext()) {
+                currentStr = scanner.nextLine();
+                arrayStr = currentStr.split("\\s");
+                if (Integer.parseInt(arrayStr[1]) < 0) {
+                    throw new Exception();
+                }
+                list.add(currentStr);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Файл не найден");
+        } catch (Exception e) {
+            System.err.println("Некорректный входной файл");
+            return null;
+        }
+        return list;
     }
 }
--- src/main/java/day14/Task3.java
+++ src/main/java/day14/Task3.java
@@ -1,7 +1,55 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        String sep = File.separator;
+        String path = "src" + sep + "test" + sep + "resources" + sep + "people.txt";
+        System.out.println(parseFileToObjList(new File(path)));
+    }
 
+    public static List<Person> parseFileToObjList(File file) {
+        List<Person> personList = new ArrayList<>();
+        String[] arrayStr;
+        String currentStr;
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNext()) {
+                currentStr = scanner.nextLine();
+                arrayStr = currentStr.split("\\s");
+                if (Integer.parseInt(arrayStr[1]) < 0) {
+                    throw new Exception();
+                }
+                personList.add(new Person(arrayStr[0], Integer.parseInt(arrayStr[1])));
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Файл не найден");
+        } catch (Exception e) {
+            System.err.println("Некорректный входной файл");
+            return null;
+        }
+        return personList;
+    }
+
+    static class Person {
+        private String name;
+        private int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
     }
 }
