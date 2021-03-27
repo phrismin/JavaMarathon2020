4b6af8493c755d070ff507bdd2efce52db12128a
Alex Rudoi <rudenw01@gmail.com>
2021-03-27 10:44:34 +0300

    solution: add tasks1-2 for day16

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,8 +1,41 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Formatter;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        String sep = File.separator;
+        String path = "src" + sep + "test" + sep + "resources" + sep + "test.txt";
+        File file = new File(path);
+        String res = printResult(file);
+        System.out.printf(res);
+    }
 
+    public static String printResult(File file) {
+        try (Scanner scanner = new Scanner(file)) {
+            double sum = 0;
+            String[] array;
+            int count = 0;
+            while (scanner.hasNext()) {
+                array = scanner.nextLine().split("\\s");
+                for (int i = 0; i < array.length; i++) {
+                    sum += Double.parseDouble(array[i]);
+                    count++;
+                }
+            }
+            double resDouble = sum / count;
+            BigDecimal bd = new BigDecimal(resDouble);
+            bd = bd.setScale(3, RoundingMode.HALF_UP);
+            double newResDouble = bd.doubleValue();
+            return resDouble + " --> " + newResDouble;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
-
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,66 @@
 package day16;
 
+import java.io.*;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        String sep = File.separator;
+        String path1 = "src" + sep + "main" + sep + "resources" + sep + "file1.txt";
+        String path2 = "src" + sep + "main" + sep + "resources" + sep + "file2.txt";
+        File file1 = new File(path1);
+        File file2 = new File(path2);
+        try {
+            BufferedWriter bw1 = new BufferedWriter(new FileWriter(file1));
+            Scanner scanner = new Scanner(new BufferedReader(new FileReader(file1)));
+            BufferedWriter bw2 = new BufferedWriter(new FileWriter(file2));
+            file1.createNewFile();
+            Random random = new Random();
+            for (int i = 0; i < 1000; i++) {
+                bw1.write(String.valueOf(random.nextInt(100)));
+                bw1.write(" ");
+            }
+            bw1.close();
+            file2.createNewFile();
+            String[] array;
+            double currentSum = 0;
+            int count = 0;
+            while (scanner.hasNext()) {
+                array = scanner.nextLine().split(" ");
+                for (String s : array) {
+                    currentSum += Integer.parseInt(s);
+                    count++;
+                    if (count == 20) {
+                        bw2.write(String.valueOf(currentSum / count));
+                        bw2.write(" ");
+                        currentSum = 0;
+                        count = 0;
+                    }
+                }
+            }
+            scanner.close();
+            bw2.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        printResult(file2);
+    }
 
+    public static void printResult(File file2) {
+        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(file2)))) {
+            String[] array;
+            double sum = 0;
+            while (scanner.hasNext()) {
+                array = scanner.nextLine().split(" ");
+                for (String s : array) {
+                    sum += Double.parseDouble(s);
+                }
+            }
+            int res = (int) sum;
+            System.out.println(res);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
