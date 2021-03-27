cd9e5a8bd790133f5dac6033caa7b138ac10ac1e
Aliaksei <phrismin@users.noreply.github.com>
2021-03-27 16:37:41 +0300

    solution: add project 'ChessBoard' for day17 tasks1-2

---
+++ src/main/java/day17/Describable.java
@@ -0,0 +1,5 @@
+package day17;
+
+public interface Describable {
+    String description();
+}
--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -2,6 +2,14 @@ package day17;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        ChessPiece[] chessPieces = {
+                ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE,
+                ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE,
+                ChessPiece.ROCK_BLACK, ChessPiece.ROCK_BLACK,
+                ChessPiece.ROCK_BLACK, ChessPiece.ROCK_BLACK
+        };
+        for (ChessPiece piece : chessPieces) {
+            System.out.print(piece.description() + " ");
+        }
     }
 }
